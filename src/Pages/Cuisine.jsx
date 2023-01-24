4e0069ab53b1de83a6e460301a1bb53af39cb52d
rsmalberca <renzo.smalberca@gmail.com>
2023-01-24 13:56:27 -0500

    cuisine component created

--- src/Pages/Cuisine.jsx
+++ src/Pages/Cuisine.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+const { REACT_APP_API_KEY } = process.env;
 
 function Cuisine() {
+  let params = useParams();
+  const [cuisine, setCuisine] = useState([]);
+  const getCuisine = async (name) => {
+    const apiData = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${REACT_APP_API_KEY}&number=15&cuisine=${name}`
+    );
+    const recipe = apiData.data.results;
+    setCuisine(recipe);
+  };
+
+  useEffect(() => {
+    getCuisine(params.category);
+  }, [params.category]);
   return <div>Cuisine</div>;
 }
 
